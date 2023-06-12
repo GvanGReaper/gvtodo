27b36686ca31cbe072640fa68be6e75a136b0a29
GvanGReaper <georgegaming.gr@gmail.com>
2023-06-13 01:36:44 +0300

    FILE CREATION WORKS WHOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- src/list.rs
+++ src/list.rs
@@ -1,4 +1,8 @@
+use std::env;
+use std::fmt::format;
 use std::fs::File;
+use std::path::{Path, self};
+
 
 use crate::config::Config;
 
@@ -13,11 +17,25 @@ impl ToDoList{
             name: name.to_string() ,
             content:  None
         };
+        let temp_string = format!("{}",config.list_folder_path);
+        let folder_path = Path::new(&temp_string);
+        let directory_path = env::current_dir().expect("Expected to get path");
 
-        let file_name = format!("{}/{}.txt",config.list_folder_path,list.name.clone());
-        match File::create(file_name.clone()){
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+        //WARNING: WONT WORK FOR ANY FILE SYSTEM THAT CONTAINS NON UTF8 COMBATIBLE CHARACTERS SINCE NO CHECKS ARE IN PLACE RIGHT NOW AND PROPABLY NEVER WILL BE
+        let temp_folder = folder_path.to_str().expect("Path should have been utf8 combatible");
+        let temp_directory = directory_path.to_str().expect("Path should have been utf8 combatible");
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        let final_path = format!("{}{}/{}",temp_directory,temp_folder,list.name);
+        // dbg!(&final_path);
+        match File::create(&final_path){
             Ok(file) => println!("{:?}", file),
-            Err(_) => println!("Unable to create the file: '{}'", file_name)
+            Err(_) => {
+                println!("Unable to create the file");
+                // dbg!("{}",&final_path);
+            }
         }
 
         return list;
